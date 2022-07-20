59d9b093d9fdf116339fc1a49316c9581a6f6100
Damien-Boukourida <IceRaag.GMG@gmail.com>
2022-07-20 12:05:45 +0200

    debut du backend pour user

---
+++ backend/src/controllers/UserController.js
@@ -0,0 +1,210 @@
+const jwt = require("jsonwebtoken");
+const models = require("../models");
+
+class UserController {
+  static browse = async (req, res) => {
+    try {
+      const [results] = await models.user.findAll();
+      return res.status(200).json(results);
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
+  };
+
+  static read = (req, res) => {
+    models.user
+      .find(req.params.id)
+      .then(([rows]) => {
+        if (rows[0] == null) {
+          res.sendStatus(404);
+        } else {
+          res.send(rows[0]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static edit = async (req, res) => {
+    const { password } = req.body;
+    const id = parseInt(req.params.id, 10);
+
+    try {
+      // TODO validations (length, format...)
+      console.warn(id, password);
+      const validUser = await models.user.validate({ id, password }, false);
+      // console.warn(validUser);
+      if (!validUser) {
+        return res.status(400).send("You must provide a valid password");
+      }
+
+      // Hash password
+      const hashedPassword = await models.user.hashPassword(password);
+
+      const [result] = await models.user.update({ id, hashedPassword });
+
+      if (result.affectedRows === 0) {
+        return res.sendStatus(404);
+      }
+
+      return res.sendStatus(204);
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
+  };
+
+  static register = async (req, res) => {
+    const { email, password, username, firstname, lastname } = req.body;
+
+    try {
+      // TODO validations (length, format...)
+      const validUser = await models.user.validate({
+        email,
+        password,
+        username,
+        firstname,
+        lastname,
+      });
+      console.warn(validUser);
+      if (!validUser) {
+        return res
+          .status(400)
+          .send("You must provide a valid email and password");
+      }
+
+      // Check if email already exists
+      const emailAlreadyUsed = await models.user.emailAlreadyExists(email);
+      if (emailAlreadyUsed) {
+        return res.status(400).send("Email already Used");
+      }
+
+      // Hash password
+      const hashedPassword = await models.user.hashPassword(password);
+
+      const [result] = await models.user.insert({
+        username,
+        firstname,
+        lastname,
+        email,
+        hashedPassword,
+      });
+      const [[userCreated]] = await models.user.find(result.insertId);
+
+      delete userCreated.hashedPassword;
+
+      return res.status(201).json(userCreated);
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
+  };
+
+  static delete = (req, res) => {
+    models.user
+      .delete(req.params.id)
+      .then(() => {
+        res.sendStatus(204);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
+  static login = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).send("You must provide an email and a password");
+    }
+
+    try {
+      const [[user]] = await models.user.findByEmail(email);
+
+      if (!user) {
+        return res.status(403).send("Invalid email or password");
+      }
+      if (await models.user.verifyPassword(password, user.hashedPassword)) {
+        // Create token
+        const token = jwt.sign({ id: user.id }, process.env.ACCESS_JWT_SECRET, {
+          expiresIn: process.env.ACCESS_JWT_EXPIRESIN,
+        });
+        return res
+          .cookie("accessToken", token, {
+            httpOnly: true,
+            secure: process.env.ACCESS_JWT_SECURE === "true",
+            maxAge: parseInt(process.env.ACCESS_JWT_COOKIE_MAXAGE, 10),
+          })
+          .status(200)
+          .json({
+            id: user.id,
+            email: user.email,
+            username: user.username,
+            firstname: user.firstname,
+          });
+      }
+
+      return res.status(403).send("Invalid email or password");
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
+  };
+
+  static authorization = (req, res, next) => {
+    const token = req.cookies.accessToken;
+    if (!token) {
+      return res.sendStatus(401);
+    }
+
+    try {
+      const decoded = jwt.verify(token, process.env.ACCESS_JWT_SECRET);
+      req.userId = decoded.id;
+      return next();
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
+  };
+
+  static clearCookie = (req, res) => {
+    return res.clearCookie("accessToken").sendStatus(200);
+  };
+
+  static isSameId = (req, res, next) => {
+    let { id } = req.params;
+
+    id = parseInt(id, 10);
+
+    if (Number.isNaN(id)) {
+      return res.status(400).send("You must provide a valid id");
+    }
+
+    if (id !== req.userId) {
+      return res.sendStatus(403);
+    }
+
+    return next();
+  };
+
+  static editAvatar = async (req, res) => {
+    // on modifie l'user id 3
+    const userId = 3;
+
+    try {
+      const [result] = await models.user.updateAvatar({
+        id: userId,
+        avatar: req.pictureData.avatar,
+        avatarDescription: req.pictureData.description,
+      });
+      if (result.affectedRows === 0) {
+        return res.status(404).send("user not found");
+      }
+      const [[userUpdated]] = await models.user.find(userId);
+      return res.status(201).json(userUpdated);
+    } catch (err) {
+      return res.status(500).send(err.message);
+    }
+  };
+}
+
+module.exports = UserController;
---
+++ backend/src/models/UserManager.js
@@ -0,0 +1,116 @@
+/* eslint-disable class-methods-use-this */
+const Joi = require("joi");
+const argon2 = require("argon2");
+const AbstractManager = require("./AbstractManager");
+
+// password must contain almost one upper case, one lower case, a number and a special character contained in [!@#$%^&*], and have 8 to 32 characters
+const schemaForCreation = Joi.object({
+  password: Joi.string()
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*.])/)
+    .min(8)
+    .max(32)
+    .required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      // tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  username: Joi.string().required(),
+  firstname: Joi.string().required(),
+  lastname: Joi.string().required(),
+});
+
+const schemaForUpdate = Joi.object({
+  id: Joi.number().required(),
+  password: Joi.string()
+    .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/)
+    .min(8)
+    .max(32)
+    .required(),
+  username: Joi.string(),
+  firstname: Joi.string(),
+  lastname: Joi.string(),
+});
+
+class UserManager extends AbstractManager {
+  static table = "user";
+
+  insert(user) {
+    return this.connection.query(
+      `insert into ${UserManager.table} (email, hashedPassword, username, firstname, lastname) values (?, ?, ?, ?, ?)`,
+      [
+        user.username,
+        user.email,
+        user.hashedPassword,
+        user.firstname,
+        user.lastname,
+      ]
+    );
+  }
+
+  update(user) {
+    return this.connection.query(
+      `update ${UserManager.table} set hashedPassword = ? where id = ?`,
+      [user.hashedPassword, user.id]
+    );
+  }
+
+  emailAlreadyExists(email) {
+    return this.connection
+      .query(`SELECT id FROM ${UserManager.table} WHERE email=?`, [email])
+      .then(([results]) => results.length);
+  }
+
+  async validate(user, creation = true) {
+    try {
+      if (creation) {
+        await schemaForCreation.validateAsync(user);
+      } else {
+        await schemaForUpdate.validateAsync(user);
+      }
+      return true;
+    } catch (err) {
+      return false;
+    }
+  }
+
+  async hashPassword(password) {
+    const hashedPassword = await argon2.hash(password);
+    return hashedPassword;
+  }
+
+  async verifyPassword(password, hashedPassword) {
+    const passwordIsValid = await argon2.verify(hashedPassword, password);
+    return passwordIsValid;
+  }
+
+  find(id) {
+    return this.connection.query(
+      `select id, username, email from  ${this.table} where id = ?`,
+      [id]
+    );
+  }
+
+  findAll() {
+    return this.connection.query(
+      `select id, username, email from  ${this.table}`
+    );
+  }
+
+  findByEmail(email) {
+    return this.connection.query(
+      `SELECT * FROM ${this.table} WHERE email = ?`,
+      [email]
+    );
+  }
+
+  updateAvatar({ id, avatar, avatarDescription }) {
+    return this.connection.query(
+      `UPDATE ${this.table} SET avatar=?, avatarDescription=? WHERE id=?`,
+      [avatar, avatarDescription, id]
+    );
+  }
+}
+
+module.exports = UserManager;
